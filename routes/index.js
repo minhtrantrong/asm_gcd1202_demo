946a5d8d19dcd428d9c03c7afff18282f2d30206
Minh Tran <ttminh@live.com>
2024-10-10 09:58:09 +0700

    add logout function

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const login_controller = require('../controllers/login_controller')
+const logout_controller = require('../controllers/logout_controller')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Cloud computing class' });
@@ -18,5 +19,9 @@ router.get('/login', function(req, res, next) {
 /* POST Login page. */
 router.post('/login', async function(req, res, next) {
   let authented = await login_controller(req, res);
+});
+/* GET /logout route */
+router.get('/logout', function(req, res, next) {
+  logout_controller(req, res);
 });
 module.exports = router;
