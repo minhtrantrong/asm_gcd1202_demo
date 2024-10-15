7b3fbeff5d3e178d3660062e4e5ae25688993d31
Minh Tran <ttminh@live.com>
2024-10-15 11:06:20 +0700

    display products table on admin page

--- controllers/get_admins_controller.js
+++ controllers/get_admins_controller.js
@@ -1,22 +1,35 @@
-function display_admin_page(req, res) {
+const pool = require("../models/pg_connector")
+async function display_admin_page(req, res) {
     if (req.session.authented && req.session.role_id <= 2) {
-        let table = `<table border=1>
-            <tr>
-                <th>Company</th>
-                <th>Contact</th>
-                <th>Country</th>
-            </tr>
-            <tr>
-                <td>Alfreds Futterkiste</td>
-                <td>Maria Anders</td>
-                <td>Germany</td>
-            </tr>
-            <tr>
-                <td>Centro comercial Moctezuma</td>
-                <td>Francisco Chang</td>
-                <td>Mexico</td>
-            </tr>
-            </table>`
+        let table = "";
+        try {
+            const query = `SELECT * FROM products`;
+            const result = await pool.query(query);
+            const rows = result.rows;
+            const fields = result.fields;
+            table = `<table border=1><tr>`;
+            // Generate header
+            let col_list = []
+            for (let field of fields) {
+                table += `<th>${field.name}</th>`;
+                col_list.push(field.name);
+            }
+            table += `</tr>`;
+            // Generate rows   
+            for (let row of rows) {
+                table +=`<tr>`;
+                for (let col of col_list) {
+                    let cell = row[col];
+                    table += `<td>${cell}</td>`;
+                }
+                table +=`</tr>`;
+            }
+            table += `</table>`;
+            
+        } catch(err) {
+            console.log(err);
+            table = "Cannot connect to DB";
+        }
         res.render('admins', { title: 'ADMIN PAGE', products_table: table });
     } else {
         res.redirect('/');
